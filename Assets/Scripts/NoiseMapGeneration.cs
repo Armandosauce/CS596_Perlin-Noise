4be3c1262d11e360d6a7375b35ae157f44b6c695
Armando Perez <armandoperez0001@gmail.com>
2019-02-23 18:37:55 -0800

    created a noisemapgeneration script that uses unity's built in perlin noise function to make a noisemap

---
+++ Assets/Scripts/NoiseMapGeneration.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseMapGeneration : MonoBehaviour
+{
+
+    public float[,] GenerateNoiseMap(int mapDepth, int mapWidth, float scale)
+    {
+        float[,] noiseMap = new float[mapDepth, mapWidth];
+
+        for (int z = 0; z < mapDepth; z++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float sX = x / scale;
+                float sZ = z / scale;
+
+                float noise = Mathf.PerlinNoise(sX, sZ);
+                noiseMap[z, x] = noise;
+            }
+        }
+        return noiseMap;
+    }
+}
