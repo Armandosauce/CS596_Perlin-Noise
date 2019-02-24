ec22766051b8188499cd7582649b3b0d4fa2baf2
Armando Perez <armandoperez0001@gmail.com>
2019-02-23 23:24:31 -0800

    added charactercontroller script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    
+    public float speed;
+    public float jumpHeight;
+    public float turnSpeed;
+    public float distToGround;
+    public float gravity;
+    public LayerMask ground;
+    public Transform checkGround;
+
+    private CharacterController _controller;
+    private Vector3 _movement;
+    private Vector3 _velocity;
+    private bool _isGrounded;
+    private bool _jump;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        
+    }
+
+    void Update()
+    {
+        resetPlayer();
+        readInput();
+        movePlayer();
+    }
+
+    void resetPlayer()
+    {
+        _isGrounded = Physics.CheckSphere(checkGround.position, distToGround, ground, QueryTriggerInteraction.Ignore);
+        if (_isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = 0f;
+        }
+    }
+    void readInput()
+    {
+        _movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    }
+
+    void movePlayer()
+    {
+        _controller.Move(_movement * Time.deltaTime * speed);
+        if(_movement != Vector3.zero)
+        {
+            this.transform.forward = _movement;
+        }
+        
+        _velocity.y += gravity * Time.deltaTime;
+        _controller.Move(_velocity * Time.deltaTime);
+
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 **Armando Perez, CS 596 Advanced Game Programming, Project 1**
-
-  **Technical Design Document**
-  Procedural Generation using Perlin Noise
+Procedural Generation using Perlin Noise
 
 The only folders needed for this project are the ones provided which are Assets and ProjectSettings. Unity will generate the rest of the folders and files.
 
-
+Additionally, there are precompiled versions of this project in the Releases folder.
 
 
 
